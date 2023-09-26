c9d5f36fdd857108dd4f7fab502d517b4a4e1f46
ryo-endo <to.endo.dev@gmail.com>
2023-09-27 00:47:14 +0900

    Add connection retry

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import asyncio
+from typing import Optional
 
-from bleak import BleakScanner, BleakClient
+from bleak import BleakScanner, BleakClient, BLEDevice
 
 DEVICE_NAME = 'M02 Pro'
 CHARACTERISTIC_UUID_WRITE = '0000ff02-0000-1000-8000-00805f9b34fb'
+
+CONNECTION_RETRY_MAX_COUNT = 5
 
 ESC = b'\x1B'
 COMMAND_FEED_PAPER = ESC + b'd'  # ESC d
@@ -11,10 +14,7 @@ COMMAND_FEED_PAPER = ESC + b'd'  # ESC d
 
 async def main():
     # scan and connect
-    print('scanning for 5 seconds, please wait...')
-    device = await BleakScanner.find_device_by_name(
-        name=DEVICE_NAME
-    )
+    device = await connect()
     if device:
         print('connected.')
     else:
@@ -28,7 +28,21 @@ async def main():
     print('disconnect.')
 
 
+async def connect() -> Optional[BLEDevice]:
+    retry_count = 0
+    device = None
+    while not device and retry_count < CONNECTION_RETRY_MAX_COUNT:
+        print(f'scanning device, please wait... ({retry_count + 1}/{CONNECTION_RETRY_MAX_COUNT})')
+        device = await BleakScanner.find_device_by_name(
+            name=DEVICE_NAME
+        )
+        retry_count += 1
+
+    return device
+
+
 async def feed(client: BleakClient, line: int = 1):
+    print(f'feed paper: {line} lines')
     await client.write_gatt_char(
         char_specifier=CHARACTERISTIC_UUID_WRITE,
         data=COMMAND_FEED_PAPER + line.to_bytes(1, 'little')
